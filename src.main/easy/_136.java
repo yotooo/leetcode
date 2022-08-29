7027dfd4f10d03f5dbc2fcd2ed4ceccfae94682d
王江潮 <youthtoday@163.com>
2022-08-29 17:34:05 +0800

    test

---
+++ src.main/easy/_136.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class _136 {
+    //set
+//    public static int singleNumber(int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            boolean flag = set.add(nums[i]);
+//            if (!flag){
+//                set.remove(nums[i]);
+//            }
+//        }
+//        return set.iterator().next();
+//    }
+
+        public static int singleNumber(int[] nums) {
+            int result =0;
+            for (int i = 0; i < nums.length; i++) {
+                result = result^nums[i];
+            }
+            return result;
+        }
+
+    public static void main(String[] args) {
+        int[] ints = {4, 1, 2, 1, 2};
+        singleNumber(ints);
+    }
+}
